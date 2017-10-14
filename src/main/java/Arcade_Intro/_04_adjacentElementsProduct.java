f09cf6626bd0f1150a4b4a58e78e83ad51bdac71
TamasNeumer <thomasneumer@gmail.com>
2017-10-14 18:54:15 +0200

    Intro 1-7

--- src/main/java/Arcade_Intro/_04_adjacentElementsProduct.java
+++ src/main/java/Arcade_Intro/_04_adjacentElementsProduct.java
@@ -2,6 +2,10 @@ package Arcade_Intro;
 
 public class _04_adjacentElementsProduct {
     int adjacentElementsProduct(int[] inputArray) {
-        return 0;
+        int answer = Integer.MIN_VALUE;
+        for(int i = 0; i < inputArray.length -1; i++) {
+            answer = inputArray[i] * inputArray[i + 1] > answer ? inputArray[i] * inputArray[i + 1] : answer;
+        }
+        return answer;
     }
 }
