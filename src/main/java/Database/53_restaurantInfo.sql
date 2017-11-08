0814efb31bff60bd02df3f55e2455cc62ce80148
TamasNeumer <thomasneumer@gmail.com>
2017-11-08 15:34:03 +0100

    SQL Until 54

---
+++ src/main/java/Database/53_restaurantInfo.sql
@@ -0,0 +1,6 @@
+CREATE PROCEDURE restaurantInfo()
+BEGIN
+    ALTER TABLE restaurants ADD COLUMN description VARCHAR(100) DEFAULT 'TBD';
+    ALTER TABLE restaurants ADD COLUMN active INT DEFAULT 1;
+    SELECT * FROM restaurants ORDER BY id;
+END
