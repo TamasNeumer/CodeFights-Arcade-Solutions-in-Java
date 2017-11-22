d01c997ab981b66f87cd8736ca46876f8a5335a1
TamasNeumer <thomasneumer@gmail.com>
2017-11-22 07:36:28 +0100

    Core Until 27

--- src/test/java/Interview/ArrayQuestionsTest.java
+++ src/test/java/Interview/ArrayQuestionsTest.java
@@ -5,9 +5,5 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayQuestionsTest {
-    @Test
-    void firstDuplicate() {
-        ArrayQuestions obj = new ArrayQuestions();
-        assertEquals(3, obj.firstDuplicate(new int[]{1}));
-    }
+
 }
